5ba60028febda6a87dffffdb299d5aac50ddba60
contability <shouldBnewB@gmail.com>
2024-06-10 18:47:07 +0900

    (add) router dom

---
+++ src/componenets/page/Root.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const PageContainer = styled.main`
+  background-color: white;
+  width: 100dvw;
+  height: 100dvh;
+  color: black;
+`;
+
+const RootPage = () => {
+  return <PageContainer>TEST</PageContainer>;
+};
+
+export default RootPage;
